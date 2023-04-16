811d5734b4de1f513dc2e76e522837e9c15a19fc
wellbeing-dough <kdw990202@naver.com>
2023-04-17 02:49:49 +0900

    단어 뒤집기 StringBuilder 사용

--- src/codeplus/algorithm_basic/begin/p9093.java
+++ src/codeplus/algorithm_basic/begin/p9093.java
@@ -2,29 +2,32 @@ package codeplus.algorithm_basic.begin;
 
 import java.io.*;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class p9093 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int t = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < t; i++) {
             String sentence = br.readLine() + "\n";
-            Stack<Character> stack = new Stack<Character>();
+            Stack<Character> stack = new Stack<>();
             for(char ch : sentence.toCharArray()) {
                 if(ch == '\n' || ch == ' ') {
                     while(!stack.empty()) {
-                        bw.write(stack.pop());
+                        sb.append(stack.pop());
+//                        bw.write(stack.pop());
                     }
-                    bw.write(ch);
+                    sb.append(ch);
+//                    bw.write(ch);
                 }else {
                     stack.push(ch);
                 }
             }
         }
-        bw.flush();
+        System.out.println(sb);
+//        bw.flush();
     }
 }
