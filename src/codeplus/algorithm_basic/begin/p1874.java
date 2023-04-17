533a14c64ff160dffeea7c42b4d13a9dec3bf89d
wellbeing-dough <kdw990202@naver.com>
2023-04-18 00:39:23 +0900

    스택 수열

---
+++ src/codeplus/algorithm_basic/begin/p1874.java
@@ -0,0 +1,42 @@
+package codeplus.algorithm_basic.begin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class p1874 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int t = Integer.parseInt(br.readLine());
+
+        int m = 0;
+
+        while(t-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+
+            if(num > m) {
+                while(num > m) {
+                    stack.push(++m);
+                    sb.append("+"+"\n");
+                }
+                stack.pop();
+                sb.append("-"+"\n");
+            }else if(num == stack.peek()) {
+                stack.pop();
+                sb.append("-"+"\n");
+            }else {
+                System.out.println("NO");
+                return;
+            }
+        }
+        System.out.println(sb);
+        br.close();
+
+    }
+}
