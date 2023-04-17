ac9cbdc78da3ff85ca60f0f7bef76f436d40941f
wellbeing-dough <kdw990202@naver.com>
2023-04-17 17:54:05 +0900

    괄호 문제

---
+++ src/codeplus/algorithm_basic/begin/p9012.java
@@ -0,0 +1,37 @@
+package codeplus.algorithm_basic.begin;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class p9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int count = 0;
+            String sentence = br.readLine();
+            for(char ch : sentence.toCharArray()) {
+                if(ch == '(') {
+                    count++;
+                }else {
+                    count--;
+                }
+                if(count < 0) {
+                    break;
+                }
+            }
+            if(count == 0) {
+                sb.append("YES\n");
+            }else {
+                sb.append("NO\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
+//그냥 앞으로 while(t-- > 0) 을 쓰자 몰라 걍 외워
