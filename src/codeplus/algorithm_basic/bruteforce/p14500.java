df7513464af201ef27ce65eb6002e03c136e663b
wellbeing-dough <kdw990202@naver.com>
2023-06-15 22:24:39 +0900

    테트로미노

--- src/codeplus/algorithm_basic/bruteforce/p14500.java
+++ src/codeplus/algorithm_basic/bruteforce/p14500.java
@@ -27,21 +27,21 @@ public class p14500 {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
 
-                if(j + 3 > m) {         //가로로 4줄
+                if(j + 3 < m) {         //가로로 4줄
                     int tmp = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i][j + 3];
                     if(ans < tmp) {
                         ans = tmp;
                     }
                 }
 
-                if(i + 3 > n) {         //새로로 4줄
+                if(i + 3 < n) {         //새로로 4줄
                     int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 3][j];
                     if(ans < tmp) {
                         ans = tmp;
                     }
                 }
 
-                if(j + 1 > m && i + 1 > n) {
+                if(j + 1 < m && i + 1 < n) {        //정사각형
                     int tmp = arr[i][j] + arr[i + 1][j] + arr[i][j + 1] + arr[i + 1][j + 1];
                     if(ans < tmp) {
                         ans = tmp;
@@ -76,7 +76,7 @@ public class p14500 {
                     }
                 }
 
-                if(j + 2 < m && i - 1 >= 0) {
+                if(j + 2 < m && i + 1 < n) {
                     int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 1][j + 2];
                     if(ans < tmp) {
                         ans = tmp;
@@ -84,11 +84,85 @@ public class p14500 {
                 }
 
                 if(j + 2 < m && i - 1 >= 0) {
-                    
+                    int tmp = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i - 1][j + 2];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
                 }
 
+                if (i + 2 < n && j - 1 >= 0) {
+                    int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 2][j - 1];
+                    if (ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j + 1 < m && i + 2 < n) {
+                    int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 2][j] + arr[i + 2][j + 1];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j + 2 < m && i - 1 >= 0) {
+                    int tmp = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i - 1][j + 1];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j + 1 < m && i + 2 < n) {
+                    int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 2][j];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j - 1 >= 0 && i + 2 < n) {
+                    int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 1][j - 1] + arr[i + 2][j];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j + 2 < m && i + 1 < n) {
+                    int tmp = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j + 1 < m && i + 2 < n) {
+                    int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 1][j + 1] + arr[i + 2][j + 1];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j - 1 >= 0 && i + 2 < n) {
+                    int tmp = arr[i][j] + arr[i + 1][j] + arr[i + 1][j - 1] + arr[i + 2][j - 1];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j + 2 < m && i - 1 >= 0) {
+                    int tmp = arr[i][j] + arr[i][j + 1] + arr[i - 1][j + 1] + arr[i - 1][j + 2];
+                    if(ans < tmp) {
+                        ans = tmp;
+                    }
+                }
+
+                if(j + 2 < m && i + 1 < n) {
+                    int tmp = arr[i][j] + arr[i][j + 1] + arr[i + 1][j + 1] + arr[i + 1][j + 2];
+                    if(ans < tmp){
+                        ans = tmp;
+                    }
+                }
             }
         }
-
+        System.out.println(ans);
     }
 }
+
+//설명할게 없다 그냥 코드 그 자체고 개빡구현
