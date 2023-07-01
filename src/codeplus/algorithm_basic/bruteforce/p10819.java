ef3b580048922e40d58bfc760eb77710329ad82e
wellbeing-dough <kdw990202@naver.com>
2023-07-01 23:34:35 +0900

    차이를 최대로

--- src/codeplus/algorithm_basic/bruteforce/p10819.java
+++ src/codeplus/algorithm_basic/bruteforce/p10819.java
@@ -4,21 +4,28 @@ import java.util.*;
 import java.io.*;
 
 public class p10819 {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(a);
         int ans = 0;
-        do {
-            int temp = calculate(a);
-            ans = Math.max(ans, temp);
-        } while(next_permutation(a));
+        while(true) {
 
-        System.out.println(ans);
+            int tmp = calculate(a);
+            ans = Math.max(ans, tmp);
+
+            if (next_permutation(a) == false) {
+                break;
+            }
+        }
+        sb.append(ans);
+        System.out.println(sb);
     }
 
     public static int calculate(int a[]) {
@@ -59,3 +66,6 @@ public class p10819 {
         return true;
     }
 }
+
+// 들어온 숫자 배열을 전부 다 하나씩 다음 순열을 구해서 그 수열을 식대로 계산해준다
+// 물론 처음 들어온 순열을 전부 sort 해줘야 모든 순열을 볼 수 있다
